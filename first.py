80f8c0e32c0057db4a0aa82f95c461e54eb49374
sin <sinahamidi8@gmail.com>
2023-10-19 19:33:27 +0330

    implement simple User Models and Routes

--- first.py
+++ first.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query, HTTPException
+from pydantic import BaseModel
+from typing import Union
+class User(BaseModel):
+    name: str
+    age: int = Path(ge=0, le=120)
+    height: Union[float, None] = None
 
+class UserIn(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserOut(BaseModel):
+    username:str
+    email:str
 
 app = FastAPI()
+@app.post('/users/')
+def get_index(user:User, car:str=Query('nothing, min_length=2, max_lenght=20')):
+    return user, car
+@app.post('/home/', response_model=UserOut, status_code=201)
+def index(usr:UserIn):
+    if usr.username == 'admin':
+        raise HTTPException(detail="user can't be admin", status_code=400, headers={"X-error":"there goes my error"})
+    return usr
 
-@app.get('/home/{name}/{age}')
-def index(name:str, age:int=0):
-    return {'message':f'{name} is {age} years old.'}
