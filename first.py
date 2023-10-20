e09d63a344bfbffe00660f73e45899ba58407288
sin <sinahamidi8@gmail.com>
2023-10-20 21:41:23 +0330

    add readuser to first

--- first.py
+++ first.py
@@ -23,3 +23,10 @@ def create_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(user)
     return user
+
+@app.get('/users/{user_id}', response_model=schemas.User)
+def read_user(user_id:int, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id==user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail='not found this user')
+    return db_user
